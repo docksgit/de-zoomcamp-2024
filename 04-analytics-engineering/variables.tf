1cf9c5c82712651a06e235241ac02478a769e76c
docksgit <rifqisyahr@gmail.com>
2024-02-15 05:43:37 +0000

    Week-4: Terraform for GCS and BQ setup and create bash script for ingestion

---
+++ 04-analytics-engineering/variables.tf
@@ -0,0 +1,43 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/home/docksgit/.gcp/de-zoomcamp-key.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "hardy-portal-411910"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "asia-southeast2-a"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "ASIA-SOUTHEAST2"
+}
+
+variable "bq_dataset_name" {
+  description = "BigQuery Dataset Name for Week-4 Homework"
+  #Update the below to what you want your dataset to be called
+  default     = "week_4_hw_dataset_docksgit"
+}
+
+variable "gcs_bucket_name" {
+  description = "Storage Bucket Name for Week-4 Homework"
+  #Update the below to a unique bucket name
+  default     = "week-4-hw-bucket-docksgit"
+}
+
+variable "gcs_bucket_dir_name" {
+  description = "Storage Bucket Directory Name for Week-4 Homework"
+  default     = "tripdata/"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
